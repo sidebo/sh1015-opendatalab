0a46f05e08d25c71b9cd9bc2c28d6b9da9e468b7
Edvin Sidebo <sidebo@kth.se>
2018-10-30 10:43:45 +0100

    adding english description and comments

--- hints/tips1.py
+++ hints/tips1.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import ipywidgets as widgets
 from IPython.display import display
-button = widgets.Button(description="Tips event-loop")
+button = widgets.Button(description="Tip event loop")
 display(button)
 
 def on_button_clicked(b):
-    print("Ett snabbsätt att loopa över händelser i ett träd är att skriva 'for evt in tree: evt.branch...'")
+    print("[SWE] Ett snabbsätt att loopa över händelser i ett träd är att skriva 'for evt in tree: evt.branch...'")
+    print("[ENG] A quick way to iterate over a tree is to write 'for evt in tree: evt.branch...'")
 
 button.on_click(on_button_clicked)
