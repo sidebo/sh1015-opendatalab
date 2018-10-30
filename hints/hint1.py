402db9cf13645232b0e7ba24abcb2cdd18d168e0
Edvin Sidebo <sidebo@kth.se>
2018-10-30 13:11:59 +0100

    Adding goodness-of-fit part in fitting. Numbering of sections. English and swedish throughout fitting

--- hints/hint1.py
+++ hints/hint1.py
@@ -5,6 +5,7 @@ button = widgets.Button(description="Tip 1 fitting")
 display(button)
 
 def on_button_clicked(b):
-    print("After calling histo.Fit(\"func\", \"S+\", ...) the fitted function can be accessed via histo.GetFunction(\"func\")" )
+    print("[SWE] Efter anropet histo.Fit(\"func\", \"S+\", ...) kan den anpassade funktionen hämtas med histo.GetFunction(\"func\")" )
+    print("[ENG] After calling histo.Fit(\"func\", \"S+\", ...) the fitted function can be accessed via histo.GetFunction(\"func\")" )
 
 button.on_click(on_button_clicked)
--- hints/hint2.py
+++ hints/hint2.py
@@ -5,6 +5,7 @@ button = widgets.Button(description="Give hint")
 display(button)
 
 def on_button_clicked(b):
-    print("The bin content and uncertainty of a histogram can be accessed with `histo.GetBinContent(i)` and `histo.GetBinError(i)`" )
+    print("[SWE] Innehållet och den statistiska osäkerheten för en bin i ett histogram kan hämtas med `histo.GetBinContent(i)` och `histo.GetBinError(i)`" )
+    print("[ENG] The bin content and statistical uncertainty of a histogram can be accessed with `histo.GetBinContent(i)` and `histo.GetBinError(i)`" )
 
 button.on_click(on_button_clicked)
