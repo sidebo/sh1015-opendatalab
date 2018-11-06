3de90ce58acf40a8eb73dc4cba30d376f8199465
Edvin Sidebo <sidebo@kth.se>
2018-11-06 11:05:51 +0100

    small fixes following Mei-Lis test of lab. Will make new Docker image

--- the-exercise/hints/hint2.py
+++ the-exercise/hints/hint2.py
@@ -5,7 +5,7 @@ button = widgets.Button(description="Give hint")
 display(button)
 
 def on_button_clicked(b):
-    print("[SWE] Innehållet och den statistiska osäkerheten för en bin i ett histogram kan hämtas med `histo.GetBinContent(i)` och `histo.GetBinError(i)`" )
-    print("[ENG] The bin content and statistical uncertainty of a histogram can be accessed with `histo.GetBinContent(i)` and `histo.GetBinError(i)`" )
+    print("[SWE] Ställ pekaren över en datapunkt för att se dess värde och dess osäkerhet." )
+    print("[ENG] Hover with the mouse over a data point to see its value and corresponding uncertainty." )
 
 button.on_click(on_button_clicked)
